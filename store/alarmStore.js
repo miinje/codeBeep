a53b23a5f3038ae35de9dfa76e897251a67e07f4
miinje <njjj00083@gmail.com>
2024-12-17 01:01:13 +0900

    Feat(alarmStore): currentTime, allAlarmData 추가 및 적용
    + react-native-background-fetch 라이브러리 삭제
    + react-native-background-actions 라이브러리 추가

--- store/alarmStore.js
+++ store/alarmStore.js
@@ -1,8 +1,12 @@
 import { create } from "zustand";
 
 const alarmStore = create((set) => ({
+  allAlarmData: null,
   isTimeMatched: false,
+  currentTime: new Date(),
+  setAllAlarmData: (state) => set({ allAlarmData: state }),
   setIsTimeMatched: (state) => set({ isTimeMatched: state }),
+  setCurrentTime: (state) => set({ currentTime: state }),
 }));
 
 export default alarmStore;
